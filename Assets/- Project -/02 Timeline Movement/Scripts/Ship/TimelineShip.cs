50ae93d147f328de6c313c4a8bfd3101b320bb06
Idris Hussain <idrisguitar@gmail.com>
2023-02-19 12:46:43 +0000

    Curve paths and distance moving past waypoints handled with loops and incremental distance changes.

--- Assets/- Project -/02 Timeline Movement/Scripts/Ship/TimelineShip.cs
+++ Assets/- Project -/02 Timeline Movement/Scripts/Ship/TimelineShip.cs
@@ -76,35 +76,74 @@ public class TimelineShip : MonoBehaviour
     {
         // Based on frame rate per second and time to mac. calculate curve time factor
         int curveTimeFactor = Mathf.RoundToInt(TimelineController.Instance.FixedFramesPerSecond * timeToMaxSpeed);
+        int currentWaypoint = 0;
         for (int i = 0; i < curveTimeFactor; i++)
         {
             if (i == 0)
             {
-                shipActionsList.Add(new TimelineShipAction(linePath[0], 0, this));
+                shipActionsList.Add(new TimelineShipAction(linePathSmoothed[currentWaypoint], 0, this));
+                currentWaypoint++;
             }
             else
             {
+                // GET DISTANCE TRAVELLED FOR FRAME
                 // Then divide current frame to save by this factor, to give curve position
                 float curvePosition = i / (float)curveTimeFactor;
                 // Run this position through the animation curve;
                 float finalCurvePosition = accelerationCurve.Evaluate(curvePosition);
                 // Get final distance to move by multiplying this by the distance to max speed.
                 float distanceMoved = finalCurvePosition * distanceToMaxSpeed;
-
-                // TODO Deal with Curved Points on Smoothed Path
-                // TODO Deal with after acceleration
-                // TODO Deal with deceleration
                 
-                // Use final direction and move towards it
-                Vector3 nextPathNode = linePath[1];
-                Vector3 dir = nextPathNode - shipActionsList[i-1].Position;
-                dir = Vector3.ClampMagnitude(dir, distanceMoved - shipActionsList[i - 1].DistanceFromStart);
-                Vector3 newShipPosition = shipActionsList[i-1].Position + dir;
+                // WORK OUT DIFFERENCE AND CHECK CURVED AND WAYPOINTS
+                // Get distance that will be travelled in this frame.
+                float distanceMovedThisFrame = distanceMoved - shipActionsList[i - 1].DistanceFromStart;
+                // Get distance between last frame position and current waypoint
+                float distanceToNextWaypoint =
+                    Vector3.Distance(shipActionsList[i - 1].Position, linePathSmoothed[currentWaypoint]);
+
+                // LOOP TO MAKE SURE If WAYPOINT IS BETWEEN MOVEMENT FRAMES
+                // For now store new position as last, then go into loop to add to it.
+                Vector3 thisFrameShipPosition = shipActionsList[i - 1].Position;
+                float distanceRemainingToGo = distanceMovedThisFrame;
+                while (distanceRemainingToGo >= distanceToNextWaypoint)
+                {
+                    // Distance will go past the way point
+                    // So we will want to move the ship part way of the way there
+                    if (currentWaypoint >= 0)
+                    {
+                        Vector3 nextPathNode = linePathSmoothed[currentWaypoint];
+                        Vector3 dir = nextPathNode - thisFrameShipPosition;
+
+
+                        // Get the remainder distance that is after the next waypoint
+                        float nextLoopDistance = distanceRemainingToGo - distanceToNextWaypoint;
+                        // Only move the amount minus the above distance past waypoint
+                        dir = Vector3.ClampMagnitude(dir, distanceRemainingToGo - nextLoopDistance);
+                        // Add this to the ship position
+                        thisFrameShipPosition += dir;
+
+                        // Then update the new distance remaining to go for the while loop condition to remainder
+                        distanceRemainingToGo = nextLoopDistance;
+                        // And set to next waypoint  
+                        currentWaypoint++;
+                    }
+                }
+                
+                // APPLY AND ADD TO TIMELINE FOR FRAME
+                // If there is a loop then this position and distance remaining to go will have been updated.
+                // If not it will just be normal from past frame.
+                Vector3 finalPathNode = linePathSmoothed[currentWaypoint];
+                Vector3 finalDir = finalPathNode - thisFrameShipPosition;
+                finalDir = Vector3.ClampMagnitude(finalDir, distanceRemainingToGo);
+                
+                // Add the movement into this frame.
                 shipActionsList.Add(new TimelineShipAction(
-                    newShipPosition, distanceMoved, this));
+                    thisFrameShipPosition + finalDir, distanceMoved, this));
             }
+            // TODO Visualise the action list on the screen for debug purposes.
+            Debug.Log("ACTION LIST COUNT " + shipActionsList.Count + " DIST:  " + shipActionsList[^1].DistanceFromStart + " WAYPOINT:  " + currentWaypoint + " LINE PATH COUNT: " + linePathSmoothed.Count);
         }
-        Debug.Log(shipActionsList.Count + " " + shipActionsList[^1].DistanceFromStart);
+  
     }
     #endregion
     
@@ -115,7 +154,6 @@ public class TimelineShip : MonoBehaviour
         if (linePath.Count == 0)
         {
             Vector3 position = transform.position;
-            linePath.Add(new Vector3(position.x, position.y + 0.01f, -9));
             linePath.Add(new Vector3(position.x + 0.5f, position.y + 0.01f, -9));
         }
     }
